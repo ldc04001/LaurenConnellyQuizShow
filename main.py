3a31b6d951f7acf5c6dc6d56e153cd33e8ec6afd
ldc04001 <114703505+ldc04001@users.noreply.github.com>
2022-10-13 23:52:54 +0000

    everything else

--- main.py
+++ main.py
@@ -14,20 +14,32 @@ print(r"""
 
 # Display the rules of the game
 print("Welcome to Quiz Show, where we ask only true or false questions. For each of the three questions, please enter the letter T or F. Good luck! ")
+print()
 
 # Store your questions and your True/False answers two tuples
 questions = ("Q1. Baltimore is the capital of Maryland", "Q2. The Mississippi River is the longest river in the world", "Q3. Michael Jackson is considered the King of Pop")
 answers = ("F", "F", "T")
 userAnswer = ""
 numberOfQuestions = len(questions)
-
 count = 0
 
-# Display each question to the user
+# Display each question to the user one at a time
 for index in range(numberOfQuestions):
   
   print(questions[index])
+  
+  while(userAnswer != "T" and userAnswer != "F"):
 
-  while(userAnswer != ("T" or "F")):
-
+    # Ask the user to put in a T or F only, or it will ask again
     userAnswer = input("Enter a 'T' for True and an 'F' for False: ")
+
+    # Keep track of the answer if it's correct
+    if(userAnswer == answers[index]):
+      count += 1
+  
+  # Reset the user's answer for each question  
+  userAnswer = ""
+  print()
+
+# the number of questions the user got right out of total number of questions.
+print(f"You got {count} out of {numberOfQuestions} correct! Thanks for playing!")
