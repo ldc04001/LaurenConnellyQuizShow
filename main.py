d2347c2e396f78314cba1f9d62d7eaa0934d4621
ldc04001 <114703505+ldc04001@users.noreply.github.com>
2022-10-13 22:45:53 +0000

    added rules

--- main.py
+++ main.py
@@ -11,3 +11,16 @@ print(r"""
  \____ $$$ \______/ |__/|________/       \______/ |__/  |__/ \______/  \_____/\___/ 
 
 """)
+
+# Display the rules of the game
+print("Welcome to Quiz Show, where we ask only true or false questions. For each of the three questions, please enter the letter T or F. Good luck! ")
+
+# Store your questions and your True/False answers two tuples
+
+# Display each question to the user
+
+# Ask for the user to anwer each question T or F
+
+# If the user doesn't answer enter a T or F, then ask them the question again
+
+# Display the number of questions they got right out of the total number of questions
